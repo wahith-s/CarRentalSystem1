9ca9f92de3e5ecc832d27c3807390e0097b037e9
wahith-s <157326704+wahith-s@users.noreply.github.com>
2024-09-01 01:13:24 +0530

    Delete CarRentalSystem directory

--- CarRentalSystem/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module CarRentalSystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.carrental.controller to javafx.fxml;
-    exports com.carrental;
-    exports com.carrental.controller;
-}
